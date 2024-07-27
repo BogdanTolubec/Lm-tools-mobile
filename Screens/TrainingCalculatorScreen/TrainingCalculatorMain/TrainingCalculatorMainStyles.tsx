630a699d75689fc0822707f7185fae6c16b1467b
BogdanTolubec <bodiabog@gmail.com>
2024-07-27 13:41:28 +0300

    commit

--- Screens/TrainingCalculatorScreen/TrainingCalculatorMain/TrainingCalculatorMainStyles.tsx
+++ Screens/TrainingCalculatorScreen/TrainingCalculatorMain/TrainingCalculatorMainStyles.tsx
@@ -33,7 +33,7 @@ const styles = StyleSheet.create(
             width: "100%",
             height: "10%",
             fontSize: 22,
-            color: "#5f9ea0",
+            color: "#5f9ea1",
             backgroundColor: "rgba(0, 0, 0, 0.6)",
         }
     }
